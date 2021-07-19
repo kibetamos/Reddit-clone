73d712abd4e15079c5677e074d750c9aa9240163
kibet kilel <amos.rotich@strathmore.edu>
2021-07-19 09:45:45 +0300

    Add the upvote and downvote function

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+  @HostBinding('attr.class') cssClass = 'row';
+  votes:number
+  title:string
+  link:string
+
+
+  constructor() { 
+    this.title = 'Angular';
+    this.link = 'http://Angular.io';
+    this.votes = 10;
+  }
+
+  voteUp(){
+    this.votes +=1;
+
+  }
+ voteDown(){
+    this.votes -=1;
+    
+  }
+
+  ngOnInit(): void {
+  }
+
+}
