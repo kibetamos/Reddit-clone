603d84e57d69a1da93f08cb804e8751e8ed73d8f
kibet kilel <amos.rotich@strathmore.edu>
2021-07-24 17:38:13 +0300

    idex.html styling and article components

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -5,7 +5,7 @@ import { Article } from './article.model';
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css']
+  styleUrls: ['./article.component.css'],
 })
 export class ArticleComponent implements OnInit {
   @HostBinding('attr.class') cssClass = 'row';
@@ -18,9 +18,17 @@ export class ArticleComponent implements OnInit {
       'http://Angular.io',
        10);
   }
-  
+  voteUp(): boolean{
+    this.article.voteUp();
+    return false;
+  }
 
-  ngOnInit(){
+  voteDown(): boolean{
+    this.article.voteDown();
+    return false;
+  }
+
+  ngOnInit() {
   }
 }
 
